2301a2d8eda63639f6378aa3ff533d036c832dd8
Catalin Dinuta <constantin.dinuta@gmail.com>
2020-12-10 10:20:05 +0200

    added install tags

--- src/main/java/com/github/dinuta/estuary/agent/model/YamlConfig.java
+++ src/main/java/com/github/dinuta/estuary/agent/model/YamlConfig.java
@@ -1,5 +1,6 @@
 package com.github.dinuta.estuary.agent.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonSetter;
 import com.fasterxml.jackson.annotation.Nulls;
@@ -9,11 +10,24 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class YamlConfig {
 
     @JsonProperty("env")
     @JsonSetter(nulls = Nulls.AS_EMPTY)
     private Map<String, String> env = new LinkedHashMap<>();
+
+    @JsonProperty("before_install")
+    @JsonSetter(nulls = Nulls.AS_EMPTY)
+    private List<String> beforeInstall = new ArrayList<>();
+
+    @JsonProperty("install")
+    @JsonSetter(nulls = Nulls.AS_EMPTY)
+    private List<String> install = new ArrayList<>();
+
+    @JsonProperty("after_install")
+    @JsonSetter(nulls = Nulls.AS_EMPTY)
+    private List<String> afterInstall = new ArrayList<>();
 
     @JsonProperty("before_script")
     @JsonSetter(nulls = Nulls.AS_EMPTY)
@@ -65,12 +79,39 @@ public class YamlConfig {
         this.env = env;
     }
 
+    public List<String> getBeforeInstall() {
+        return beforeInstall;
+    }
+
+    public void setBeforeInstall(List<String> beforeInstall) {
+        this.beforeInstall = beforeInstall;
+    }
+
+    public List<String> getInstall() {
+        return install;
+    }
+
+    public void setInstall(List<String> install) {
+        this.install = install;
+    }
+
+    public List<String> getAfterInstall() {
+        return afterInstall;
+    }
+
+    public void setAfterInstall(List<String> afterInstall) {
+        this.afterInstall = afterInstall;
+    }
+
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("{\n");
 
         sb.append("    env: ").append(toIndentedString(env));
+        sb.append("    before_install: ").append(toIndentedString(beforeInstall));
+        sb.append("    install: ").append(toIndentedString(install));
+        sb.append("    after_install: ").append(toIndentedString(afterInstall));
         sb.append("    before_script: ").append(toIndentedString(beforeScript));
         sb.append("    script: ").append(toIndentedString(script));
         sb.append("    after_script: ").append(toIndentedString(afterScript));

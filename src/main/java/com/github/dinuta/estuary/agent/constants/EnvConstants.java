7e3d99b3e56d0a2a230bd37bcc4f7b50e66e6e5e
Catalin Dinuta <43060213+dinuta@users.noreply.github.com>
2021-02-19 10:04:56 +0200

    added back the auth through Token header (#31)

--- src/main/java/com/github/dinuta/estuary/agent/constants/EnvConstants.java
+++ src/main/java/com/github/dinuta/estuary/agent/constants/EnvConstants.java
@@ -1,6 +1,7 @@
 package com.github.dinuta.estuary.agent.constants;
 
 public class EnvConstants {
+    public static final String HTTP_AUTH_TOKEN = "HTTP_AUTH_TOKEN";
     public static final String HTTP_AUTH_USER = "HTTP_AUTH_USER";
     public static final String HTTP_AUTH_PASSWORD = "HTTP_AUTH_PASSWORD";
     public static final String FLUENTD_IP_PORT = "FLUENTD_IP_PORT";

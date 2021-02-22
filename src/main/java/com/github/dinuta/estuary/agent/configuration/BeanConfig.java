233e1ba4662c665e1b9120b4becaf4f642283575
Catalin Dinuta <constantin.dinuta@gmail.com>
2021-02-22 16:39:18 +0200

    corrected bug for fluentd logging

--- src/main/java/com/github/dinuta/estuary/agent/configuration/BeanConfig.java
+++ src/main/java/com/github/dinuta/estuary/agent/configuration/BeanConfig.java
@@ -1,12 +1,17 @@
 package com.github.dinuta.estuary.agent.configuration;
 
+import com.github.dinuta.estuary.agent.component.About;
 import com.github.dinuta.estuary.agent.model.StateHolder;
 import com.github.dinuta.estuary.agent.model.api.CommandParallel;
+import com.github.dinuta.estuary.agent.service.FluentdService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BeanConfig {
+    @Autowired
+    private About about;
 
     @Bean
     public CommandParallel getCommandParallel() {
@@ -16,5 +21,10 @@ public class BeanConfig {
     @Bean
     public StateHolder getStateHolder() {
         return new StateHolder();
+    }
+
+    @Bean
+    public FluentdService getFluentdService() {
+        return new FluentdService(about);
     }
 }
